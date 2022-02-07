11ab5be232adbb4f66d1f735dce3e551d428a34c
Arilson F J Silva <arilsonsilva61@gmail.com>
2022-02-06 21:25:18 -0300

    implementado servico pesquisarId, precisa corrigir

--- src/main/java/br/arilson/estoque/dao/DaoCategorias.java
+++ src/main/java/br/arilson/estoque/dao/DaoCategorias.java
@@ -1,6 +1,7 @@
 package br.arilson.estoque.dao;
 
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 import br.arilson.estoque.entidades.Categorias;
 
@@ -17,27 +18,52 @@ public class DaoCategorias {
 			ps.execute();
 		}
 	}
-	
+
 	public static void excluir(int idCategorias) throws Exception {
-		
+
 		String slq = "DELETE FROM CATEGORIAS WHERE idCategorias = ? ";
-		
-		try (PreparedStatement ps = DB.connect().prepareStatement(slq)){
-			
+
+		try (PreparedStatement ps = DB.connect().prepareStatement(slq)) {
+
 			ps.setInt(1, idCategorias);
 			ps.execute();
 		}
 	}
-	
+
 	public static void atualizar(Categorias categorias) throws Exception {
-		
-		String sql = "UPDATE CATEGORIAS SET nome = ? WHERE idCategorias = ?"; 
-		
-		try(PreparedStatement ps = DB.connect().prepareStatement(sql)){
-			
+
+		String sql = "UPDATE CATEGORIAS SET nome = ? WHERE idCategorias = ?";
+
+		try (PreparedStatement ps = DB.connect().prepareStatement(sql)) {
+
 			ps.setString(1, categorias.getNome());
 			ps.setInt(2, categorias.getIdCategorias());
 			ps.executeUpdate();
 		}
+	}
+
+	public static Categorias pesquisar(int idCategorias) throws Exception {
+
+		String sql = "SELECT * FROM CATEGORIAS WHERE idCategorias = ?";
+
+		try (PreparedStatement ps = DB.connect().prepareStatement(sql)) {
+
+			ps.setInt(1, idCategorias);
+			ResultSet rs = ps.executeQuery();
+
+			if (rs.next()) {
+				Categorias categorias = new Categorias();
+				
+				categorias.setNome(rs.getString("nome"));
+				categorias.setIdCategorias(rs.getInt("idCategorias"));
+				
+
+				return categorias;
+			}else {
+				return null;
+			}
+
+		} 
+		
 	}
 }
--- src/main/java/br/arilson/estoque/servicos/ServicoCategorias.java
+++ src/main/java/br/arilson/estoque/servicos/ServicoCategorias.java
@@ -46,6 +46,7 @@ public class ServicoCategorias {
 	@Produces(MediaType.APPLICATION_JSON)
 	@Consumes(MediaType.APPLICATION_JSON)
 	public void atualizar(Categorias categorias) {
+		
 		try {
 			DaoCategorias.atualizar(categorias);
 		}catch (Exception e) {
@@ -55,23 +56,22 @@ public class ServicoCategorias {
 	
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Path("pesquisarId")
+	public Categorias pesquisarId(@QueryParam("idCategorias") int idCategorias) {
+
+		try {
+			DaoCategorias.pesquisar(idCategorias);
+		}catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return null;
+	}
+	
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
 	public List<Categorias> listar(){
-		return null;
-	}
-	
-	@GET
-	@Produces(MediaType.APPLICATION_JSON)
-	@Consumes(MediaType.APPLICATION_JSON)
-	@Path("pesquisar")
-	public List<Categorias> listar(@QueryParam("pesquisar") String pesquisar){
-		return null;
-	}
-	
-	@GET
-	@Produces(MediaType.APPLICATION_JSON)
-	@Consumes(MediaType.APPLICATION_JSON)
-	@Path("pesquisarID")
-	public Categorias pesquisarID(@QueryParam("codigo") int codigo) {
 		return null;
 	}
 }
